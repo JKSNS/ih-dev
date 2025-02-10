07cfd0467e5dd8fa28a9da943173cd160a950350
Jackson Stephens <156037195+JKSNS@users.noreply.github.com>
2025-02-10 13:05:22 -0700

    Update linux_harden.sh

--- linux_harden.sh
+++ linux_harden.sh
@@ -310,6 +310,9 @@ function disable_other_firewalls {
     # fi
 }
 
+########################################################################
+# FUNCTION: setup_ufw
+# Configures UFW firewall rules.
 function setup_ufw {
     print_banner "Configuring ufw"
 
@@ -333,62 +336,306 @@ function setup_ufw {
     fi
 }
 
-function setup_iptables {
-    # TODO: this needs work/testing on different distros
-    print_banner "Configuring iptables"
-    echo "[*] Installing iptables packages"
+########################################################################
+# FUNCTION: setup_custom_iptables
+# This function writes the entire provided IPtables hardening script to a
+# temporary file and executes it.
+function setup_custom_iptables {
+    print_banner "Configuring iptables (Custom Script)"
+    tmpfile=$(mktemp /tmp/iptables_script.XXXXXX)
+    cat <<'EOF' > "$tmpfile"
+#!/bin/bash
 
-    if [ "$pm" == 'apt' ]; then
-        # Debian and Ubuntu
-        sudo "$pm" install -y iptables iptables-persistent #ipset
-        SAVE='/etc/iptables/rules.v4'
-    else
-        # Fedora
-        sudo "$pm" install -y iptables-services
-        sudo systemctl enable iptables
-        sudo systemctl start iptables
-        SAVE='/etc/sysconfig/iptables'
-    fi
+# Below is a valid bash script that is also a valid awk script (albeit one that does nothing)
+# Its only purpose is to run this file as an awk script with the proper `ss` command piped in
+# on the off chance it isn't run properly
 
-    # echo "[*] Creating private ip range ipset"
-    # sudo ipset create PRIVATE-IP hash:net
-    # sudo ipset add PRIVATE-IP 10.0.0.0/8
-    # sudo ipset add PRIVATE-IP 172.16.0.0/12
-    # sudo ipset add PRIVATE-IP 192.168.0.0/16
-    # sudo ipset save | sudo tee /etc/ipset.conf
-    # sudo systemctl enable ipset
+#region bash
+sh -c "ss -napH4 | awk -f $BASH_SOURCE" {0..0}
+"exit" {0..0}
+#endregion bash
 
-    echo "[*] Creating INPUT rules"
-    sudo iptables -P INPUT DROP
-    sudo iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-    sudo iptables -A INPUT -i lo -j ACCEPT
-    sudo iptables -A INPUT -s 0.0.0.0/0 -j ACCEPT
+# Tested `ss` options:
+# - napOH4
+# - napH4
 
-    echo "[*] Which ports should be open for incoming traffic (INPUT)?"
-    echo "[*] Warning: Do NOT forget to add 22/SSH if needed- please don't accidentally lock yourself out of the system!"
-    ports=$(get_input_list)
-    for port in $ports; do
-        sudo iptables -A INPUT --dport "$port" -j ACCEPT
-    done
-    # TODO: is there a better alternative to this rule?
-    sudo iptables -A INPUT -j LOG --log-prefix "[iptables] CHAIN=INPUT ACTION=DROP "
+# Configuration
+BEGIN {
+  # Competition Configuration
 
-    echo "[*] Creating OUTPUT rules"
-    # TODO: harden this as much as possible, like by limiting destination hosts
-    # sudo iptables -P OUTPUT DROP
-    # sudo iptables -A OUTPUT -o lo -j ACCEPT
-    # sudo iptables -A OUTPUT -p tcp -m multiport --dport 80,443 -m set ! --match-set PRIVATE-IP dst -j ACCEPT
-    # Web traffic
-    sudo iptables -A OUTPUT -p tcp -m multiport --dport 80,443 -j WEB
-    sudo iptables -N WEB
-    sudo iptables -A WEB -d 10.0.0.0/8,172.16.0.0/12,192.168.0.0/16 -j LOG --log-prefix "[iptables] WEB/private ip "
-    sudo iptables -A WEB -j ACCEPT
-    # DNS traffic
-    sudo iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
+  # Space-seperated list of subnets that are unrestricted (VPN, white team, etc.)
+  UNRESTRICTED_SUBNETS = "10.128.XXX.0/24";
 
-    echo "[*] Saving rules"
-    sudo iptables-save | sudo tee $SAVE
+  EXTERNAL_SUBNET = "10.120.XXX.0/24";  # UNUSED
+
+  # Space-seperated list of DNS server IPs
+  DNS_SERVERS = "192.168.XXX.1 192.168.XXX.2";
+
+  # Script Configuration
+  IPTABLES_CMD = "iptables";
+  DEFAULT_INPUT_CHAIN = "INPUT";
+  DEFAULT_OUTPUT_CHAIN = "OUTPUT";
+  
+  # Logging
+  # 1 : Debug
+  # 2 : Info
+  # 3 : Warning
+  # 4 : Error
+  LOG_LEVEL = 2;
+
+  # Set to 1 to skip the warning at the beginning of the script
+  SKIP_PROMPT = 0;
+
+  # Space-seperated list of inbound connection types
+  # Default: "LISTEN" for TCP and "UNCONN" for UDP
+  INBOUND_CONNECTION_TYPES = "LISTEN";
+
+  # Space-seperated list of outbound connection types
+  # Default: "ESTAB" for TCP and UDP
+  OUTBOUND_CONNECTION_TYPES = "ESTAB";
+
+  # Port Whitelist
+  # Used to only allow ports that should have a service
+  INBOUND_PORT_WHITELIST  = "21 22 80 443 53"
+  OUTBOUND_PORT_WHITELIST = INBOUND_PORT_WHITELIST
+
+  # Colored output
+  COLORED_OUTPUT = 1;
 }
+
+function keyify(arr) {
+  for(i in arr) arr[arr[i]] = 1;
+}
+
+# Variables / Constants
+BEGIN {
+  # Get arrays from config vars
+  split(INBOUND_CONNECTION_TYPES, INBOUND_CONNECTION_TYPES_ARRAY);
+  keyify(INBOUND_CONNECTION_TYPES_ARRAY);
+  split(OUTBOUND_CONNECTION_TYPES, OUTBOUND_CONNECTION_TYPES_ARRAY);
+  keyify(OUTBOUND_CONNECTION_TYPES_ARRAY);
+  
+  split(INBOUND_PORT_WHITELIST, INBOUND_PORT_WHITELIST_ARRAY);
+  keyify(INBOUND_PORT_WHITELIST_ARRAY);
+  split(OUTBOUND_PORT_WHITELIST, OUTBOUND_PORT_WHITELIST_ARRAY);
+  keyify(OUTBOUND_PORT_WHITELIST_ARRAY);
+
+  # Declare array of colors
+  delete COLORS[0];
+
+  # Fill the array full of delicious colors
+  COLORS["black"]         = 30;
+  COLORS["red"]           = 31;
+  COLORS["green"]         = 32;
+  COLORS["yellow"]        = 33;
+  COLORS["blue"]          = 34;
+  COLORS["magenta"]       = 35;
+  COLORS["cyan"]          = 36;
+  COLORS["white"]         = 37;
+  COLORS["default"]       = 39;
+
+  COLORS["gray"]          = 90;
+  COLORS["brightRed"]     = 91;
+  COLORS["brightGreen"]   = 92;
+  COLORS["brightYellow"]  = 93;
+  COLORS["brightBlue"]    = 94;
+  COLORS["brightMagenta"] = 95;
+  COLORS["brightCyan"]    = 96;
+  COLORS["brightWhite"]   = 97;
+
+  # Declare array of log colors
+  delete LOG_LEVEL_COLORS[0];
+
+  LOG_LEVEL_COLORS["DEBUG"]   = "magenta";
+  LOG_LEVEL_COLORS["INFO"]    = "cyan";
+  LOG_LEVEL_COLORS["WARNING"] = "yellow";
+  LOG_LEVEL_COLORS["ERROR"]   = "red";
+
+  # Log Level Names
+  delete LOG_LEVEL_NAMES[0];
+
+  LOG_LEVEL_NAMES["DEBUG"]   = 1;
+  LOG_LEVEL_NAMES["INFO"]    = 2;
+  LOG_LEVEL_NAMES["WARNING"] = 3;
+  LOG_LEVEL_NAMES["ERROR"]   = 4;
+}
+
+function setColor(color) {
+  if(!COLORED_OUTPUT)
+    return "";
+  else if(color == "")
+    return "\033[39m";
+  else
+    return sprintf("\033[%dm", COLORS[color]);
+}
+
+function colored(color, message) {
+  return setColor(color) message setColor();
+}
+
+function printLog(level, message) {
+  # If the chosen log level is above the level of this message, don't display it
+  if(LOG_LEVEL_NAMES[level] < LOG_LEVEL) return;
+  # The 17 comes from the length of the ANSI escape codes for color plus the max log level length (7 + 10)
+  printf("%*s: %s\n", COLORED_OUTPUT ? 17 : 7, colored(LOG_LEVEL_COLORS[level], level), message) > "/dev/tty";
+}
+
+function formatPort(port, proto, name) {
+  return colored("yellow", sprintf("%5d", port)) "/" proto " (" colored("blue", name) ")";
+}
+
+# Base Ruleset and housekeeping
+BEGIN {
+  "tty" | getline isTTY;
+  if(isTTY != "not a tty")
+    printLog("WARNING", "TTY stdin detected. This script is designed to take the output from `ss -napOH4`");
+
+  if(!SKIP_PROMPT) {
+    # This is a prompt, not a log
+    print(colored("red", "WARNING") ": Existing rules for the " DEFAULT_INPUT_CHAIN " and " DEFAULT_OUTPUT_CHAIN " chains will be flushed!\nPress RETURN to continue.") > "/dev/tty";
+    getline < "/dev/tty";
+  }
+
+  # Remove the previous ruleset
+  printLog("INFO", "Flushing " DEFAULT_INPUT_CHAIN);
+  print(IPTABLES_CMD, "-F", DEFAULT_INPUT_CHAIN);
+  printLog("INFO", "Flushing " DEFAULT_OUTPUT_CHAIN);
+  print(IPTABLES_CMD, "-F", DEFAULT_OUTPUT_CHAIN);
+
+  # Loopback traffic
+  printLog("INFO", "Accepting loopback traffic on " DEFAULT_INPUT_CHAIN);
+  print(IPTABLES_CMD, "-A", DEFAULT_INPUT_CHAIN,  "-i lo -j ACCEPT");
+  printLog("INFO", "Accepting loopback traffic on " DEFAULT_OUTPUT_CHAIN);
+  print(IPTABLES_CMD, "-A", DEFAULT_OUTPUT_CHAIN, "-o lo -j ACCEPT");
+
+  # Connection Tracking
+  printLog("INFO", "Enabling connection tracking on " DEFAULT_INPUT_CHAIN);
+  print(IPTABLES_CMD, "-A", DEFAULT_INPUT_CHAIN,  "-m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT");
+  printLog("INFO", "Enabling connection tracking on " DEFAULT_OUTPUT_CHAIN);
+  print(IPTABLES_CMD, "-A", DEFAULT_OUTPUT_CHAIN, "-m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT");
+
+  # ICMP
+  printLog("INFO", "Accepting ICMP traffic from " DEFAULT_INPUT_CHAIN);
+  print(IPTABLES_CMD, "-A", DEFAULT_INPUT_CHAIN, "-p icmp -j ACCEPT");
+
+  # Outbound to the internet
+  # print(IPTABLES_CMD, "-A", DEFAULT_INPUT_CHAIN, "-d", EXTERNAL_SUBNET, "-j ACCEPT");
+
+  # DNS
+  split(DNS_SERVERS, _DNS_SERVERS);
+  for(server in _DNS_SERVERS) {
+    printLog("INFO", "New outbound rule - " sprintf("%15s", _DNS_SERVERS[server]) ":" colored("yellow", sprintf("%5d", 53)) "/udp (" colored("blue", "DNS") ")");
+    print(IPTABLES_CMD, "-A", DEFAULT_OUTPUT_CHAIN, "-p udp -m udp --dport 53 -d", _DNS_SERVERS[server], "-j ACCEPT");
+  }
+
+  split(UNRESTRICTED_SUBNETS, _UNRESTRICTED_SUBNETS);
+  for(subnet in _UNRESTRICTED_SUBNETS) {
+    printLog("INFO", "New inbound rule - " sprintf("%18s", _UNRESTRICTED_SUBNETS[subnet]) " (" colored("blue", "unrestricted subnet") ")");
+    print(IPTABLES_CMD, "-A", DEFAULT_INPUT_CHAIN, "-d", _UNRESTRICTED_SUBNETS[subnet], "-j ACCEPT");
+  }
+
+  # Initialize tables of added rules
+  delete INPUT_RULES[0];
+  delete OUTPUT_RULES[0];
+}
+
+function extractIP(string) {
+  if(match(string, /[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/)) {
+    return substr(string,RSTART,RLENGTH);
+  }
+  return "";
+}
+
+# Runs before other rules to process common fields
+{
+  # Process info
+  if(match($7, "\"[^\"]+\""))
+    name = substr($7,RSTART + 1,RLENGTH - 2);
+  else
+    name = "";
+     
+  # Remote ports and IPs
+  split($6, remote, ":");
+  remoteIP   = extractIP(remote[1]);
+  if(remoteIP == "")
+    printLog("WARNING", "Invalid remote IP for " colored("yellow", $6) " (" colored("blue", name) ")");
+  if(remote[2] != "*" && (remote[2] < 1 || remote[2] > 65535))
+    printLog("WARNING", "Invalid remote port for " colored("yellow", $6) " (" colored("blue", name) ")");
+
+  # Local ports and IPs
+  split($5, local, ":");
+  localIP   = extractIP(local[1]);
+  if(localIP == "")
+    printLog("WARNING", "Invalid local IP for "   colored("yellow", $5) " (" colored("blue", name) ")");
+  if(local[2] != "*" && (local[2] < 1 || local[2] > 65535))
+    printLog("WARNING", "Invalid local port for " colored("yellow", $5) " (" colored("blue", name) ")");
+}
+
+# Listening processes
+$2 in INBOUND_CONNECTION_TYPES_ARRAY {
+  # Skip if the rule has already been created
+  if(INPUT_RULES[local[2] "/" $1]) next;
+
+  # Skip adding the rule if the port isn't in the whitelist
+  if(local[2] in INBOUND_PORT_WHITELIST_ARRAY == 0) {
+    printLog("WARNING", "Inbound connection " formatPort(local[2], $1, name) " not in whitelist, skipping.");
+    next;
+  }
+
+  # Comment (if process info is present)
+  if(name) comment = "-m comment --comment \"" name "\"";
+  else     comment = ""
+
+  # Actual command
+  print(IPTABLES_CMD " -A " DEFAULT_INPUT_CHAIN " -p " $1 " -m " $1 " --dport " local[2] " " comment " -j ACCEPT");
+
+  # Log
+  printLog("INFO", "New inbound rule - " formatPort(local[2], $1, name));
+
+  # Record the port and protocol so we can avoid duplicating rules
+  INPUT_RULES[local[2] "/" $1] = 1;
+}
+
+# Established connections
+$2 in OUTBOUND_CONNECTION_TYPES_ARRAY {
+  # Skip if the rule has already been created
+  if(remoteIP) {
+    if(OUTPUT_RULES[$6 "/" $1]) next;
+  } else {
+    if(OUTPUT_RULES[remote[2] "/" $1]) next;
+  }
+  
+  # Skip adding the rule if the port isn't in the whitelist
+  if(remote[2] in OUTBOUND_PORT_WHITELIST_ARRAY == 0) {
+    printLog("WARNING", "Outbound connection " formatPort(remote[2], $1, name) " not in whitelist, skipping.");
+    next;
+  }
+
+  # Destination IP
+  if(remoteIP) remoteIPMatcher = "-d " remoteIP;
+  else         remoteIPMatcher = ""
+
+  # Comment (if process info is present)
+  if(name) comment = "-m comment --comment \"" name "\"";
+  else     comment = ""
+
+  # iptables command
+  print(IPTABLES_CMD " -A " DEFAULT_OUTPUT_CHAIN " -p " $1 "-m " $1 " --dport " remote[2] " " remoteIPMatcher " -j ACCEPT");
+
+  # Log the rule creation
+  printLog("INFO", "New outbound rule - " sprintf("%15s", remoteIP) ":" formatPort(remote[2], $1, name));
+
+  # Record the port, IP, and protocol so we can avoid duplicating rules
+  if(remoteIP) OUTPUT_RULES[$6 "/" $1] = 1;
+  else         OUTPUT_RULES[remote[2] "/" $1] = 1;
+}
+EOF
+    chmod +x "$tmpfile"
+    sudo "$tmpfile"
+    rm "$tmpfile"
+}
+
+########################################################################
 
 function backups {
     print_banner "Backups"
@@ -725,28 +972,100 @@ function harden_web {
     # Additional user security measures (e.g., auditing hidden users) can be added here.
 }
 
-######################## MAIN #######################
+########################################################################
+# MENU FUNCTION
+# This function displays a menu of major parts of the script. The user may choose to run:
+#   1) The full hardening process
+#   2) User management (create users, change passwords, disable users, remove sudoers)
+#   3) Firewall configuration (choose UFW or custom iptables)
+#   4) Backups
+#   5) Splunk installation
+#   6) SSH hardening
+#   7) PAM/profile fixes and system configuration (fix PAM, remove profiles, check permissions, sysctl config)
+#   8) Web hardening
+#   9) Exit
+function show_menu {
+    print_banner "Hardening Script Menu"
+    echo "1) Full Hardening Process (Run all)"
+    echo "2) User Management"
+    echo "3) Firewall Configuration"
+    echo "4) Backups"
+    echo "5) Splunk Installation"
+    echo "6) SSH Hardening"
+    echo "7) PAM/Profile Fixes & System Config"
+    echo "8) Web Hardening"
+    echo "9) Exit"
+    echo
+    read -p "Enter your choice [1-9]: " menu_choice
+    echo
+    case $menu_choice in
+        1)
+            main ;;
+        2)
+            detect_system_info
+            install_prereqs
+            create_ccdc_users
+            change_passwords
+            disable_users
+            remove_sudoers ;;
+        3)
+            detect_system_info
+            install_prereqs
+            disable_other_firewalls
+            echo "[*] Choose a firewall configuration method:"
+            echo "    1) UFW"
+            echo "    2) iptables (Custom)"
+            read -p "Enter your choice [1-2]: " fw_choice
+            case $fw_choice in
+                1) setup_ufw ;;
+                2) setup_custom_iptables ;;
+                *) echo "[X] Invalid choice. Defaulting to UFW."; setup_ufw ;;
+            esac ;;
+        4)
+            backups ;;
+        5)
+            setup_splunk ;;
+        6)
+            secure_ssh ;;
+        7)
+            fix_pam
+            remove_profiles
+            check_permissions
+            sysctl_config ;;
+        8)
+            harden_web ;;
+        9)
+            echo "Exiting..."; exit 0 ;;
+        *)
+            echo "Invalid option. Exiting."
+            exit 1 ;;
+    esac
+}
+
+########################################################################
+# MAIN FUNCTION: Runs the full hardening process
 function main {
     echo "CURRENT TIME: $(date +"%Y-%m-%d_%H:%M:%S")"
-    echo "[*] Start of script"
-
+    echo "[*] Start of full hardening process"
     detect_system_info
     install_prereqs
-
     create_ccdc_users
     change_passwords
     disable_users
     remove_sudoers
-
     disable_other_firewalls
-    setup_ufw
-    # setup_iptables
-
+    # --- FIREWALL CONFIGURATION SECTION ---
+    echo "[*] Choose a firewall configuration method:"
+    echo "    1) UFW"
+    echo "    2) iptables (Custom)"
+    read -p "Enter your choice [1-2]: " fw_choice
+    case $fw_choice in
+        1) setup_ufw ;;
+        2) setup_custom_iptables ;;
+        *) echo "[X] Invalid choice. Defaulting to UFW."; setup_ufw ;;
+    esac
     backups
     setup_splunk
-        
-	
-    # updates below: 
     secure_ssh
     remove_profiles
     fix_pam
@@ -755,23 +1074,29 @@ function main {
     patch_vulnerabilities
     check_permissions
     sysctl_config
-
-    echo "[*] End of script"
+    echo "[*] End of full hardening process"
     echo "[*] Script log can be viewed at $LOG"
     echo "[*] ***Please install system updates now***"
+    # After full hardening, optionally ask about web hardening.
+    web_choice=$(get_input_string "Would you like to perform web hardening? (y/N): ")
+    if [ "$web_choice" == "y" ]; then
+        harden_web
+    fi
 }
 
-# Parse arguments
+########################################################################
+# PARSE ARGUMENTS (e.g., --debug)
 for arg in "$@"; do
     case "$arg" in
         --debug )
             echo "[*] Debug mode enabled"
             debug="true"
-        ;;
+            ;;
     esac
 done
 
-# Set up logging
+########################################################################
+# SET UP LOGGING DIRECTORY
 LOG_PATH=$(dirname "$LOG")
 if [ ! -d "$LOG_PATH" ]; then
     sudo mkdir -p "$LOG_PATH"
@@ -779,11 +1104,6 @@ if [ ! -d "$LOG_PATH" ]; then
     sudo chmod 750 "$LOG_PATH"
 fi
 
-# Run main function and log output
-main "$@" 2>&1 | sudo tee -a $LOG
-
-# After main, prompt the user whether to perform web hardening.
-web_choice=$(get_input_string "Would you like to harden web? (y/N): ")
-if [ "$web_choice" == "y" ]; then
-    harden_web
-fi
+########################################################################
+# MAIN EXECUTION: Instead of automatically running the full script, show the menu.
+show_menu
